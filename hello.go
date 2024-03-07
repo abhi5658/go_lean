b58f7a9b3d481ef0df80f90e9e89da16bbda1244
Abhishek Shah <abhishekshah5658@gmail.com>
2024-03-07 23:51:19 +0530

    add unexported function

--- hello.go
+++ hello.go
@@ -9,9 +9,14 @@ import (
 func main() {
 	fmt.Println("message is: ", quote.Go())
 	fmt.Println(Hello("abhishek"))
+  fmt.Println("sum for 2 and 3 is: ", add(2,3))
 }
 
-func Hello(name string) string {
+func Hello(name string) string { // exported as function name starts with capital letter
 	message := fmt.Sprintf("hello %v. How are you?", name)
 	return message
+}
+
+func add(x int, y int) int { // unexported as function name starts with small letter
+	return x + y
 }

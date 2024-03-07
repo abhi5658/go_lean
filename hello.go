10cfdcf077b032961850a1b5edf2f0ba5a01ca10
Abhishek Shah <abhishekshah5658@gmail.com>
2024-03-07 23:44:47 +0530

    add new exported function with capital letter

--- hello.go
+++ hello.go
@@ -5,6 +5,13 @@ import (
 
 	"rsc.io/quote"
 )
+
 func main() {
-    fmt.Println(quote.Go())
+	fmt.Println("message is: ", quote.Go())
+	fmt.Println(Hello("abhishek"))
+}
+
+func Hello(name string) string {
+	message := fmt.Sprintf("hello %v. How are you?", name)
+	return message
 }

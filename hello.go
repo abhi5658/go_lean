f1f70cc5e2ccea767be2cb79cf197ac1a846c02c
Abhishek Shah <abhishekshah5658@gmail.com>
2024-03-08 00:10:16 +0530

    add naked function

--- hello.go
+++ hello.go
@@ -18,6 +18,11 @@ func add(x, y int) int { // unexported as function name starts with small letter
 func swap(x, y string) (string, string) {
 	return y, x
 }
+func nakedFunc(sum int) (x, y int) {
+	x = sum / 4
+	y = sum + 4
+	return
+}
 
 func main() {
 	fmt.Println("message is: ", quote.Go())
@@ -25,10 +30,15 @@ func main() {
 	fmt.Println("sum for 2 and 3 is: ", add(2, 3))
 	a, b := swap("string-a", "string-b")
 	fmt.Println("after swapped", a, b)
+	c, d := nakedFunc(16)
+	fmt.Println("naked return is:", c, d)
 }
 
-// $> go run .
-// message is:  Don't communicate by sharing memory, share memory by communicating.
-// hello abhishek. How are you?
-// sum for 2 and 3 is:  5
-// after swapped string-b string-a
+/*
+$> go run .
+message is:  Don't communicate by sharing memory, share memory by communicating.
+hello abhishek. How are you?
+sum for 2 and 3 is:  5
+after swapped string-b string-a
+naked return is: 4 20
+*/
